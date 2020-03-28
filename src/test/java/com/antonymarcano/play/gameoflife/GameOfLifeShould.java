2e71d78489ff511615e45b156e5ce0facaaa0f88
Antony Marcano <antony@riverglide.com>
2020-03-28 13:25:01 +0000

    WIP: The board should have no live cells after being seeded with only two live cells - cells with only one neighbour die
    **    We would not usually commit a failing test   **
    ** This is included only to illustrate the process **
    
    NOTE: The approach being taken is inspired by 'TDD as if you meant it'.
    In this approach, the implementation goes in the test-method until there's a good reason for it to be extracted.

--- src/test/java/com/antonymarcano/play/gameoflife/GameOfLifeShould.java
+++ src/test/java/com/antonymarcano/play/gameoflife/GameOfLifeShould.java
@@ -1,5 +1,6 @@
 package com.antonymarcano.play.gameoflife;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.HashSet;
@@ -16,6 +17,21 @@ public class GameOfLifeShould {
     public void have_no_live_cells_when_seeded_with_only_one_live_cell() {
         Set<LiveCell> board = of(
                 LiveCell.at(0,0)
+        );
+
+        Set<LiveCell> draftBoard = new HashSet<>(board);
+        draftBoard.remove(LiveCell.at(0,0));
+        board = copyOf(draftBoard);
+
+        assertThat(board.size(), is(0));
+    }
+
+    @Test
+    @Ignore("Failing Test to illustrate step by step process in commits. Comment out @Ignore line to see it fail.")
+    public void have_no_live_cells_when_seeded_with_only_two_live_cells() {
+        Set<LiveCell> board = of(
+                LiveCell.at(0,0),
+                LiveCell.at(0,1)
         );
 
         Set<LiveCell> draftBoard = new HashSet<>(board);
