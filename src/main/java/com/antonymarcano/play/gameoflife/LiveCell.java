7ab9f1b37ffa0562d2b1bff4f932e36bc2c6cbfe
Antony Marcano <antony@riverglide.com>
2020-03-28 14:14:21 +0000

    NEW: The board stays unchanged after being seeded with four neighbouring cells, up and right of each other – cells with three neighbours live.

--- src/main/java/com/antonymarcano/play/gameoflife/LiveCell.java
+++ src/main/java/com/antonymarcano/play/gameoflife/LiveCell.java
@@ -3,17 +3,26 @@ package com.antonymarcano.play.gameoflife;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode
-@SuppressWarnings({"unused", "FieldCanBeLocal"})//because they're needed for @EqualsAndHashCode but the IDE can't tell they're being used
 public class LiveCell {
     private final int x;
     private final int y;
 
+    public static LiveCell at(int x, int y) {
+        return new LiveCell(x,y);
+    }
     public LiveCell(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public static LiveCell at(int x, int y) {
-        return new LiveCell(x,y);
+    public int x() { return x; }
+    public int y() { return y; }
+
+    @Override
+    public String toString() {
+        return "LiveCell{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
     }
 }
--- src/test/java/com/antonymarcano/play/gameoflife/GameOfLifeShould.java
+++ src/test/java/com/antonymarcano/play/gameoflife/GameOfLifeShould.java
@@ -1,8 +1,8 @@
 package com.antonymarcano.play.gameoflife;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.AbstractMap.SimpleEntry;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,7 +16,7 @@ public class GameOfLifeShould {
     @Test
     public void have_no_live_cells_when_seeded_with_only_one_live_cell() {
         Set<LiveCell> board = of(
-                LiveCell.at(0,0)
+                LiveCell.at(0, 0)
         );
 
         board = nextGenerationOf(board);
@@ -27,8 +27,8 @@ public class GameOfLifeShould {
     @Test
     public void have_no_live_cells_when_seeded_with_only_two_live_cells() {
         Set<LiveCell> board = of(
-                LiveCell.at(0,0),
-                LiveCell.at(0,1)
+                LiveCell.at(0, 0),
+                LiveCell.at(0, 1)
         );
 
         board = nextGenerationOf(board);
@@ -37,13 +37,12 @@ public class GameOfLifeShould {
     }
 
     @Test
-    @Ignore("Failing Test to illustrate step by step process in commits. Comment out @Ignore line to see it fail.")
     public void stay_unchanged_when_four_cells_have_three_live_neighbours_up_and_right() {
         Set<LiveCell> board = of(
-                LiveCell.at(0,0),
-                LiveCell.at(0,1),
-                LiveCell.at(1,1),
-                LiveCell.at(1,0)
+                LiveCell.at(0, 0),
+                LiveCell.at(0, 1),
+                LiveCell.at(1, 1),
+                LiveCell.at(1, 0)
         );
         Set<LiveCell> stillLifeBlock = copyOf(board);
 
@@ -55,7 +54,29 @@ public class GameOfLifeShould {
 
     private Set<LiveCell> nextGenerationOf(Set<LiveCell> board) {
         Set<LiveCell> draftBoard = new HashSet<>(board);
-        draftBoard.removeAll(board);
+        Set<SimpleEntry<Integer, Integer>> potentialNeighbours = of(
+                new SimpleEntry<>(0, 1), //top middle
+                new SimpleEntry<>(1, 1), //top right
+                new SimpleEntry<>(1, 0), //middle right
+                new SimpleEntry<>(1, -1), //bottom right
+                new SimpleEntry<>(-1, 0), //bottom middle
+                new SimpleEntry<>(-1, -1), //bottom left
+                new SimpleEntry<>(0, -1), //middle left
+                new SimpleEntry<>(-1, 1)  //top left
+        );
+
+        for (LiveCell cell : board) {
+            int count = 0;
+            for (SimpleEntry<Integer, Integer> neighbour : potentialNeighbours) {
+                int x = cell.x() + neighbour.getKey();
+                int y = cell.y() + neighbour.getValue();
+                LiveCell potentialNeighbour = new LiveCell(x, y);
+
+                if (draftBoard.contains(potentialNeighbour)) count++;
+            }
+            if (count == 3) continue;
+            draftBoard.remove(cell);
+        }
         board = copyOf(draftBoard);
         return board;
     }
