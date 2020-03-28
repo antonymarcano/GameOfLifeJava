466c4d4c0abec5ab273ca711786243b1a5d17152
Antony Marcano <antony@riverglide.com>
2020-03-28 16:55:06 +0000

    REFACTOR WIP: Exploring the idea of a neighbourhood that knows its size

--- src/test/java/com/antonymarcano/play/gameoflife/neighbourhood/NeighbourhoodShould.java
+++ src/test/java/com/antonymarcano/play/gameoflife/neighbourhood/NeighbourhoodShould.java
@@ -2,6 +2,7 @@ package com.antonymarcano.play.gameoflife.neighbourhood;
 
 import com.antonymarcano.play.gameoflife.GameOfLife;
 import com.antonymarcano.play.gameoflife.LiveCell;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -21,5 +22,17 @@ public class NeighbourhoodShould {
 
         Neighbours neighbours = Neighbourhood.of(cell).on(board);
         assertThat(neighbours.size(), is(8));
+    }
+
+    @Test
+    @Ignore("Failing Test to illustrate step by step process in commits. Comment out @Ignore line to see it fail.")
+    public void have_size_of_zero_when_a_cell_is_surrounded() {
+        GameOfLife board = mock(GameOfLife.class);
+        when(board.contains(any(LiveCell.class))).thenReturn(false);
+
+        LiveCell cell = LiveCell.at(0,0);
+
+        Neighbours neighbours = Neighbourhood.of(cell).on(board);
+        assertThat(neighbours.size(), is(0));
     }
 }
