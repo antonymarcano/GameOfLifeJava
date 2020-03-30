9fbbb160c2c4d8c0bbaa8f63b46a215ada4f0a68
Antony Marcano <antony@riverglide.com>
2020-03-30 06:29:30 +0100

    WIP: Clients of the LiveCell don't need to know the meaning of the value it's given to decide its survival

--- src/test/java/com/antonymarcano/play/gameoflife/cell/LiveCellShould.java
+++ src/test/java/com/antonymarcano/play/gameoflife/cell/LiveCellShould.java
@@ -1,7 +1,8 @@
 package com.antonymarcano.play.gameoflife.cell;
 
 import com.antonymarcano.play.gameoflife.neighbourhood.Community;
-import com.antonymarcano.play.gameoflife.neighbourhood.Neighbourhood;
+import com.antonymarcano.play.gameoflife.neighbourhood.StillNeedsACell;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -11,41 +12,50 @@ import static org.mockito.Mockito.when;
 
 public class LiveCellShould {
 
+    private StillNeedsACell neighbourhood;
+    private Community community;
+    private LiveCell cell;
+
+    @Before
+    public void establishCommunity() {
+        cell = LiveCell.at(0, 0);
+
+        neighbourhood = mock(StillNeedsACell.class);
+        community = mock(Community.class);
+
+        when(neighbourhood.ofGiven(cell)).thenReturn(community);
+    }
+
     @Test
     public void survive_with_three_live_neighbours_excluding_itself() {
-        LiveCell cell = LiveCell.at(0, 0);
+        when(community.population()).thenReturn(4);
 
-        assertThat(cell.isAllowedToLiveIn(4), is(true));
+        assertThat(cell.isAllowedToLiveIn(neighbourhood), is(true));
     }
 
     @Test
     public void survive_with_two_live_neighbours_excluding_itself() {
-        LiveCell cell = LiveCell.at(0, 0);
+        when(community.population()).thenReturn(3);
 
-        assertThat(cell.isAllowedToLiveIn(3), is(true));
+        assertThat(cell.isAllowedToLiveIn(neighbourhood), is(true));
     }
 
     @Test
     public void not_survive_with_less_than_two_live_neighbours_excluding_itself() {
-        LiveCell cell = LiveCell.at(0, 0);
+        when(community.population()).thenReturn(2);
 
-        assertThat(cell.isAllowedToLiveIn(2), is(false));
+        assertThat(cell.isAllowedToLiveIn(neighbourhood), is(false));
     }
 
     @Test
     public void not_survive_with_more_than_three_live_neighbours_excluding_itself() {
-        LiveCell cell = LiveCell.at(0, 0);
+        when(community.population()).thenReturn(5);
 
-        assertThat(cell.isAllowedToLiveIn(5), is(false));
+        assertThat(cell.isAllowedToLiveIn(neighbourhood), is(false));
     }
 
     @Test
     public void know_whether_it_should_live_in_its_neighbourhood() {
-        LiveCell cell = LiveCell.at(0, 0);
-
-        Neighbourhood neighbourhood = mock(Neighbourhood.class);
-        Community community = mock(Community.class);
-        when(neighbourhood.ofGiven(cell)).thenReturn(community);
         when(community.population()).thenReturn(3);
 
         assertThat(cell.isAllowedToLiveIn(neighbourhood), is(true));
